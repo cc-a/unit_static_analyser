a5964035a726ff050a63f5f54a551b4b68313eb8
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2025-09-11 15:06:07 +0100

    Refactor to use console script rather than __main__

--- unit_static_analyser/__init__.py
+++ unit_static_analyser/__init__.py
@@ -2,6 +2,31 @@
 
 from contextlib import suppress
 from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
+
+from .mypy_checker.checker import UnitChecker
 
 with suppress(PackageNotFoundError):
     __version__ = version(__name__)
+
+
+def run() -> None:
+    """Run the unit checker on provided filenames."""
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: python -m unit_static_analyser <file1.py> <file2.py> ...")
+        sys.exit(1)
+
+    checker = UnitChecker()
+    checker.check(
+        [path for path_str in sys.argv[1:] if (path := Path(path_str)).exists()]
+    )
+
+    print("Unit checking completed.")
+    print(f"Errors: {checker.errors}")
+    for key, val in checker.var_units.items():
+        print(f"{key.fullname}: {val}")
+    print(f"Func units: {checker.function_units}")
+    for unit in checker.function_units.values():
+        print(unit)
--- unit_static_analyser/__main__.py
+++ unit_static_analyser/__main__.py
@@ -1,26 +1,6 @@
 """Entrypoint that runs against any filenames provided on the command line."""
 
-from pathlib import Path
-
-from .mypy_checker.checker import UnitChecker
-
-
-def run() -> None:
-    """Run the unit checker on provided filenames."""
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Usage: python -m unit_static_analyser <file1.py> <file2.py> ...")
-        sys.exit(1)
-
-    checker = UnitChecker()
-    checker.check(
-        [path for path_str in sys.argv[1:] if (path := Path(path_str)).exists()]
-    )
-
-    print("Unit checking completed.")
-    print(f"Errors: {checker.errors}")
-
+from . import run
 
 if __name__ == "__main__":
     run()
